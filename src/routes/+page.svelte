ca5135b0549de6996e8f16aa098853a78eb20108
AleksandrZorge <alekszorgedev@gmail.com>
2024-03-31 17:19:06 +0300

    add styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,44 @@
     </div>
   </div>
 </main>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  main {
+    background-color: rgb(78, 78, 78);
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .calculator {
+    background-color: rgb(28, 28, 28);
+    width: 240px;
+    padding: 15px;
+    border-radius: 7px;
+  }
+  .results {
+    height: 60px;
+    color: white;
+    font-size: 50px;
+    display: flex;
+    flex-direction: row-reverse;
+    margin-right: 10px;
+  }
+  .digits {
+    display: flex;
+  }
+  .operations {
+    display: flex;
+    flex-direction: column;
+  }
+  .numbers {
+    display: flex;
+    flex-wrap: wrap;
+    width: 200px;
+  }
+</style>
